1f3ffa41e673546003e0243dcbbaf3e050b3cf54
Tanveer <tanveershaikh.3101@gmail.com>
2024-07-02 19:13:42 +0530

    Added functionality to add new books and remove existing books

--- index.js
+++ index.js
@@ -8,29 +8,24 @@ function Books(title, author, pages, isRead) {
     }
 }
 
-Books.prototype.myName = () => {
-    return `Tanveer`;
-}
+let addBook = document.querySelector(".addBook");
+let refresh = document.querySelector(".refreshTable");
+let table = document.querySelector("#bookTable");
+
 
 
 let Hobbit = new Books("Hobbit", "Hobbiter", 920, true);
 let ReviveYourHeart = new Books("Revive Your Heart", "NAK", 600, false);
+let index;
+bookArr = [Hobbit,ReviveYourHeart];
 
-bookArr = [Hobbit, ReviveYourHeart]
 
-// let alert1 = document.querySelector(".alert1");
-// alert1.addEventListener("click", () => {
 
-// })
-
-// for (let book of objarray) {
-//     alert(book.info());
-// }
-
-let table = document.querySelector("#bookTable");
 function fillTable() {
-    for (let book of bookArr) {
+    header();
+    bookArr.forEach((book, i) => {
         let row = table.insertRow();
+        row.id = i;
         let cell1 = row.insertCell(0);
         cell1.innerHTML = book.title;
         let cell2 = row.insertCell(1);
@@ -38,10 +33,69 @@ function fillTable() {
         let cell3 = row.insertCell(2);
         cell3.innerHTML = book.pages;
         let cell4 = row.insertCell(3);
-        cell4.innerHTML = book.isRead?"Read":"Not Read";
-    }
+        cell4.innerHTML = book.isRead ? "Read" : "Not Read";
+        let cell5 = row.insertCell(4);
+        cell5.innerHTML = `<td><button class='removeButton' id=${i}>Remove</button></td>`;
+    });
 }
 
+function addToTable() {
+    let newTitle = document.querySelector("#newBookTitle");
+    let newAuthor = document.querySelector("#newBookAuthor");
+    let newPages = document.querySelector("#newBookPages");
+    let newStatus = document.getElementsByName("status");
 
-fillTable()
+    if (newTitle.value == ""){
+        return alert("Enter Book Name");
+    }
+    else if (! newStatus[0].checked && ! newStatus[1].checked) {
+        return;
+    }
+    
+    let readStatus;
+        
+    if (newStatus[0].checked) {
+        readStatus = true;
+    }
+    if(newStatus[1].checked){
+        readStatus = false;
+    }
+    let newBook = newTitle.value;
+    
+    newBook = new Books(newTitle.value, newAuthor.value, newPages.value, readStatus);
+    bookArr.push(newBook);
+    console.log(bookArr);
+}
+
+//adding records and repopulating entire table
+addBook.addEventListener("click", (e) => {
+    e.preventDefault()
+    addToTable();
+    fillTable();
+})
+
+//Filling the entire table
+refresh.addEventListener("click", (e) => {
+    e.preventDefault()
+    fillTable();
+})
+
+//Code to create a Header every time a table is filled
+function header() {
+    table.innerHTML = "<tr><th>Title</th><th>Author</th><th>No. of Pages</th><th>Read Status</th><th>Remove</th></tr>";
+}
+
+//Remove button Code
+$(document).ready(function(){
+    $('body').on('click', '.removeButton', function(){
+        if($('.removeButton').length >= 0){
+            $(this).parents('tr').remove();
+            bookArr.splice(this.id, 1);
+            fillTable();
+            console.log(bookArr);
+        }
+    });
+});
+
+fillTable();
 
